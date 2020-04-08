438800cecaea4322a18dededc27065dcc6a9eb2c
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-08 10:52:05 +0100

    Hibernate : base OK -> Spring Boot AmigosCode

--- src/main/java/com/oc/moko/oc_lade/OcLadeApplication.java
+++ src/main/java/com/oc/moko/oc_lade/OcLadeApplication.java
@@ -1,13 +1,46 @@
 package com.oc.moko.oc_lade;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.oc.moko.oc_lade.model.Utilisateur;
 
 @SpringBootApplication
 public class OcLadeApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(OcLadeApplication.class, args);
+		
+		Utilisateur nouvelUtilisateur = new Utilisateur();
+		nouvelUtilisateur.setIdUtilisateur(1L);
+		nouvelUtilisateur.setPrenomUtilisateur("Federico");
+		nouvelUtilisateur.setNomUtilisateur("Desmoulin");
+		
+		Utilisateur utilisateur = new Utilisateur();
+		
+		Configuration configuration = new Configuration().configure().addAnnotatedClass(Utilisateur.class);
+		
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		
+		SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		
+		Session session = sessionFactory.openSession();
+		
+		Transaction transaction = session.beginTransaction();
+		
+		session.save(nouvelUtilisateur);
+		
+		utilisateur = (Utilisateur) session.get(Utilisateur.class, 1L);
+		
+		transaction.commit();
+		
+		System.out.println(utilisateur);
 	}
-
 }
--- src/main/java/com/oc/moko/oc_lade/model/Utilisateur.java
+++ src/main/java/com/oc/moko/oc_lade/model/Utilisateur.java
@@ -1,34 +1,66 @@
 package com.oc.moko.oc_lade.model;
 
-import java.util.UUID;
+import javax.persistence.Column;
 
+//import java.ustil.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name="tb_utilisateur")
 public class Utilisateur {
 	
-	private UUID id_utilisateur;
-	private String prenom_utilisateur;
-	private String nom_utilisateur;
-	
-	public UUID getId_utilisateur() {
-		return id_utilisateur;
+	@Id
+	@Column(name="id_utilisateur")
+	private Long idUtilisateur;
+	@Column(name="prenom_utilisateur")
+	private String prenomUtilisateur;
+	@Column(name="nom_utilisateur")
+	private String nomUtilisateur;
+
+	public Utilisateur() {
+	}
+
+	public Utilisateur(
+		@JsonProperty("idUtilisateur") Long idUtilisateur,
+		@JsonProperty("prenomUtilisateur") String prenomUtilisateur,
+		@JsonProperty("nomUtilisateur") String nomUtilisateur
+	) {
+		this.idUtilisateur = idUtilisateur;
+		this.prenomUtilisateur = prenomUtilisateur;
+		this.nomUtilisateur = nomUtilisateur;
+	}
+
+	public Long getIdUtilisateur() {
+		return idUtilisateur;
 	}
 	
-	public void setId_utilisateur(UUID id_utilisateur) {
-		this.id_utilisateur = id_utilisateur;
+	public void setIdUtilisateur(Long idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
 	}
 	
-	public String getPrenom_utilisateur() {
-		return prenom_utilisateur;
+	public String getPrenomUtilisateur() {
+		return prenomUtilisateur;
 	}
 	
-	public void setPrenom_utilisateur(String prenom_utilisateur) {
-		this.prenom_utilisateur = prenom_utilisateur;
+	public void setPrenomUtilisateur(String prenomUtilisateur) {
+		this.prenomUtilisateur = prenomUtilisateur;
 	}
 	
-	public String getNom_utilisateur() {
-		return nom_utilisateur;
+	public String getNomUtilisateur() {
+		return nomUtilisateur;
 	}
 	
-	public void setNom_utilisateur(String nom_utilisateur) {
-		this.nom_utilisateur = nom_utilisateur;
+	public void setNomUtilisateur(String nomUtilisateur) {
+		this.nomUtilisateur = nomUtilisateur;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", prenomUtilisateur=" + prenomUtilisateur + ", nomUtilisateur=" + nomUtilisateur + "]";
 	}
 }
